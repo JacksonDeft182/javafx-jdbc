0bfd2b8930b7d297c7b60bcaaa8ca7b93488fb73
Jackson Alves <jack182@outlook.com.br>
2022-03-25 13:49:23 -0300

    Salvando Vendedores

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,13 +1,16 @@
 package gui;
 
 import java.net.URL;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -124,21 +127,41 @@ public class SellerFormController implements Initializable {
 	}
 
 	private Seller getFormData() {
-		Seller objDep = new Seller();
+		Seller objSeller = new Seller();
 
 		ValidationException exception = new ValidationException("Validation error");
 
-		objDep.setId(Utils.tryParseToInt(txtId.getText()));
+		objSeller.setId(Utils.tryParseToInt(txtId.getText()));
 
 		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
-			exception.addError("Name", "Field can't be empty");
+			exception.addError("name", "Field can't be empty");
 		}
-		objDep.setName(txtName.getText());
-
+		objSeller.setName(txtName.getText());
+		
+		if (txtEmail.getText() == null || txtEmail.getText().trim().equals("")) {
+			exception.addError("email", "Field can't be empty");
+		}
+		objSeller.setEmail(txtEmail.getText());
+		
+		if(dpBirthDate.getValue() == null) {
+			exception.addError("birthDate","Field can't be empty");
+		}
+		else {
+			Instant instant = Instant.from(dpBirthDate.getValue().atStartOfDay(ZoneId.systemDefault())); // Forma de pegar uma data de um DatePicker FX
+			objSeller.setBirthDate(Date.from(instant));
+		}
+		
+		if (txtBaseSalary.getText() == null || txtBaseSalary.getText().trim().equals("")) {
+			exception.addError("baseSalary", "Field can't be empty");
+		}
+		objSeller.setBaseSalary(Utils.tryParseToDouble(txtBaseSalary.getText()));
+		
+		objSeller.setDepartment(comboBoxDepartment.getValue());
+		
 		if (exception.getErrors().size() > 0) {
 			throw exception;
 		}
-		return objDep;
+		return objSeller;
 	}
 
 	@FXML
@@ -197,7 +220,12 @@ public class SellerFormController implements Initializable {
 	}
 
 	private void setErrorMessages(Map<String, String> errors) {
-		labelErrorName.setText(errors.get("Name"));
+		Set<String> fields = errors.keySet();
+		
+		labelErrorName.setText(fields.contains("name") ? errors.get("name") : "");
+		labelErrorEmail.setText(fields.contains("email") ? errors.get("email") : "");
+		labelErrorBaseSalary.setText(fields.contains("baseSalary") ? errors.get("baseSalary") : "");
+		labelErrorBirthDate.setText(fields.contains("birthDate") ? errors.get("birthDate") : "");
 	}
 
 	private void initializeComboBoxDepartment() {
--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -27,6 +27,14 @@ public class Utils {
 			return null;
 		}
 	}
+	
+	public static Double tryParseToDouble(String str) {
+		try {
+			return Double.parseDouble(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 
 	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
 		tableColumn.setCellFactory(column -> {
@@ -92,4 +100,6 @@ public class Utils {
 			}
 		});
 	}
+	
+	
 }
